fe30f875ab1d36c5a8d8e0b168a4f2834bd8cfcd
Brian Maddy <brian@brianmaddy.com>
2017-12-26 22:56:23 -0600

    moved parsing code to separate file

---
+++ src/hlt/parser.clj
@@ -0,0 +1,83 @@
+(ns hlt.parser
+  (:require [hlt.entity :as e]))
+
+(def docking-status-id->docking-status
+  [:undocked :docking :docked :undocking])
+
+(defn get-entities
+  [get-entity [entity-count & data]]
+  (loop [entity-num 0
+         data data
+         entities []]
+    (if (= entity-num entity-count)
+      [entities data]
+      (let [[entity remaining] (get-entity data)]
+        (recur (inc entity-num)
+               remaining
+               (conj entities entity))))))
+
+(defn get-ship
+  [owner-id
+   [id
+    x y
+    health
+    _ ;; x-velocity is deprecated
+    _ ;; y-velocity is deprecated
+    docking-status-id
+    docked-planet-id
+    docking-progress
+    weapon-cooldown
+    & remaining]]
+  (let [ship {:id id
+              :owner-id owner-id
+              :position [x y]
+              :radius e/ship-radius
+              :health health
+              :docking-status (docking-status-id->docking-status docking-status-id)
+              :docked-planet-id docked-planet-id
+              :weapon-cooldown weapon-cooldown}]
+    [ship remaining]))
+
+(defn get-player
+  [[player-id & data]]
+  (let [[ships remaining] (get-entities #(get-ship player-id %) data)]
+    [{:id player-id :ships ships} remaining]))
+
+(defn get-planet
+  [[id
+    x y
+    health
+    radius
+    docking-spot-count
+    current-production
+    _ ;; remaining-production is deprecated
+    has-owner
+    owner-candidate
+    docked-ship-count
+    & data]]
+  (let [[docked-ship-ids remaining] (split-at docked-ship-count data)
+        planet {:id id
+                :position [x y]
+                :radius radius
+                :docking-spot-count docking-spot-count
+                :health health
+                :owner-id (condp = has-owner
+                            0 nil
+                            1 owner-candidate)
+                :docked-ship-ids docked-ship-ids}]
+    [planet remaining]))
+
+(defn build-game-map
+  [data]
+  (let [[ships remaining] (get-entities get-player data)
+        [planets _] (get-entities get-planet remaining)
+        ships-by-player-id (reduce (fn [acc {:keys [id ships]}]
+                                     (assoc acc id ships))
+                                   {}
+                                   ships)
+        planets-by-id (reduce (fn [acc {:keys [id] :as planet}]
+                                (assoc acc id planet))
+                              {}
+                              planets)]
+    {:ships-by-player-id ships-by-player-id
+     :planets-by-id planets-by-id}))
--- src/system.clj
+++ src/system.clj
@@ -3,7 +3,7 @@
             [clojure.java.io :as io]
             [taoensso.timbre :as timbre :refer [info]]
             [taoensso.timbre.appenders.core :as appenders]
-            [hlt.entity :as e]
+            [hlt.parser :as parser]
             clojure.pprint)
   (:gen-class))
 
@@ -19,94 +19,11 @@
   []
   (edn/read-string (str \[ (read-line) \])))
 
-(def docking-status-id->docking-status
-  [:undocked :docking :docked :undocking])
-
-(defn get-entities
-  [get-entity [entity-count & data]]
-  (loop [entity-num 0
-         data data
-         entities []]
-    (if (= entity-num entity-count)
-      [entities data]
-      (let [[entity remaining] (get-entity data)]
-        (recur (inc entity-num)
-               remaining
-               (conj entities entity))))))
-
-(defn get-ship
-  [owner-id
-   [id
-    x y
-    health
-    _ ;; x-velocity is deprecated
-    _ ;; y-velocity is deprecated
-    docking-status-id
-    docked-planet-id
-    docking-progress
-    weapon-cooldown
-    & remaining]]
-  (let [ship {:id id
-              :owner-id owner-id
-              :position [x y]
-              :radius e/ship-radius
-              :health health
-              :docking-status (docking-status-id->docking-status docking-status-id)
-              :docked-planet-id docked-planet-id
-              :weapon-cooldown weapon-cooldown}]
-    [ship remaining]))
-
-(defn get-player
-  [[player-id & data]]
-  (let [[ships remaining] (get-entities #(get-ship player-id %) data)]
-    [{:id player-id :ships ships} remaining]))
-
-(defn get-planet
-  [[id
-    x y
-    health
-    radius
-    docking-spot-count
-    current-production
-    _ ;; remaining-production is deprecated
-    has-owner
-    owner-candidate
-    docked-ship-count
-    & data]]
-  (let [[docked-ship-ids remaining] (split-at docked-ship-count data)
-        planet {:id id
-                :position [x y]
-                :radius radius
-                :docking-spot-count docking-spot-count
-                :health health
-                :owner-id (condp = has-owner
-                            0 nil
-                            1 owner-candidate)
-                :docked-ship-ids docked-ship-ids}]
-    [planet remaining]))
-
-(defn build-game-map
-  [data]
-  (let [[ships remaining] (get-entities get-player data)
-        [planets _] (get-entities get-planet remaining)
-        ships-by-player-id (reduce (fn [acc {:keys [id ships]}]
-                                     (assoc acc id ships))
-                                   {}
-                                   ships)
-        planets-by-id (reduce (fn [acc {:keys [id] :as planet}]
-                                (assoc acc id planet))
-                              {}
-                              planets)]
-    {:ships-by-player-id ships-by-player-id
-     :planets-by-id planets-by-id}))
-
 (defn start
   [bot]
   (let [[tag] (get-line)
-        _ (info {:tag tag})
         map-size (get-line)
-        _ (info {:map-size map-size})
-        state (assoc (build-game-map (get-line))
+        state (assoc (parser/build-game-map (get-line))
                      :tag tag
                      :map-size map-size)]
     (info state))
