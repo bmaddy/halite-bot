5e773f2aaf3bd541c4aa172ca2cfe60db3bbe06c
Brian Maddy <brian@brianmaddy.com>
2017-12-31 23:52:50 -0600

    remove commented out stuff

--- project.clj
+++ project.clj
@@ -3,11 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "All Rights Reserved"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 ;; [org.clojure/tools.logging "0.4.0"]
-                 ;; [ch.qos.logback/logback-classic "1.1.3"]
-                 [com.taoensso/timbre "4.10.0"]
-                 ]
-  ;; :main ^:skip-aot MyBot
+                 [com.taoensso/timbre "4.10.0"]]
   :main ^:skip-aot system
   :target-path "target/"
   :uberjar-name "MyBot.jar"
