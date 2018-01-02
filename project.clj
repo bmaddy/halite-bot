f2d61ea6bac789b0342b458b108db40dbe025b5f
Brian Maddy <brian@brianmaddy.com>
2018-01-02 12:39:23 -0600

    made ships go to different planets

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "All Rights Reserved"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.taoensso/timbre "4.10.0"]]
+                 [com.taoensso/timbre "4.10.0"]
+                 [org.clojure/math.combinatorics "0.1.4"]]
   :main ^:skip-aot MyBot
   :target-path "target/"
   :uberjar-name "MyBot.jar"
--- src/bot_impl.clj
+++ src/bot_impl.clj
@@ -1,7 +1,9 @@
 (ns bot-impl
   (:require [hlt.entity :as e]
             [hlt.navigation :as nav]
-            [hlt.bot :as bot]))
+            [hlt.bot :as bot]
+            [clojure.math.combinatorics :as combo]
+            [taoensso.timbre :as timbre]))
 
 (defn compute-move
   [ship state]
@@ -13,10 +15,37 @@
          (e/dock-move ship planet)
          (nav/navigate-to-dock ship planet state))))))
 
+(defn default-strategy
+  [{:keys [player-id] :as state}]
+  (let [ships (get-in state [:ships-by-player-id player-id])]
+    (keep #(compute-move % state) ships)))
+
+(defn all-pairings
+  "Returns all possible pairings of ships and planets."
+  [ships planets]
+  (set
+   (for [ss (combo/permutations ships)
+         ps (combo/permutations planets)]
+     (zipmap ss ps))))
+
+(defn target-planet
+  [state ship planet]
+  (if (e/within-docking-range? ship planet)
+    (e/dock-move ship planet)
+    (nav/navigate-to-dock ship planet state)))
+
+(defn nearest-planet-first
+  [{:keys [player-id] :as state}]
+  (let [ships (get-in state [:ships-by-player-id player-id])
+        grouped-planets (->> state :planets-by-id vals (group-by (comp boolean :has-owner)))
+        [owned unowned] (map grouped-planets [true false])]
+    (remove nil? (map (partial target-planet state) ships (cycle unowned)))))
+
+;; get all ship/planet combinations for undocked ships
+;; min-by sum of the distance
+;; build the moves
+
 (def bot
   (reify bot/IBot
     (get-name [this {:keys [player-id] :as state}] (str "basic-bot-" player-id))
-    (next-moves [this {:keys [player-id] :as state}]
-      (let [ships (get-in state [:ships-by-player-id player-id])]
-        (keep #(compute-move % state) ships)))))
-
+    (next-moves [this {:keys [player-id] :as state}] (nearest-planet-first state))))
