ade02104be707feeaeb20bbacca9a57a75741505
Brian Maddy <brian@brianmaddy.com>
2018-01-02 23:00:37 -0600

    improved bot strategy

--- src/bot_impl.clj
+++ src/bot_impl.clj
@@ -2,6 +2,7 @@
   (:require [hlt.entity :as e]
             [hlt.navigation :as nav]
             [hlt.bot :as bot]
+            [hlt.math :as math]
             [clojure.math.combinatorics :as combo]
             [taoensso.timbre :as timbre]))
 
@@ -20,13 +21,35 @@
   (let [ships (get-in state [:ships-by-player-id player-id])]
     (keep #(compute-move % state) ships)))
 
-(defn all-pairings
+(defn all-distances
   "Returns all possible pairings of ships and planets."
   [ships planets]
-  (set
-   (for [ss (combo/permutations ships)
-         ps (combo/permutations planets)]
-     (zipmap ss ps))))
+  (for [ship ships
+        planet planets]
+    {:distance (math/distance-between ship planet)
+     :ship ship
+     :planet planet}))
+
+(defn pick-closest
+  [distances]
+  (if (empty? distances)
+    {}
+    (let [{:keys [ship planet]} (first (sort-by :distance distances))
+          remaining (remove (fn [d]
+                              (or (= ship (:ship d)) (= planet (:planet d))))
+                            distances)]
+      (assoc (pick-closest remaining)
+             ship planet))))
+
+(defn repeatedly-pick-closest
+  [distances]
+  (if (empty? distances)
+    {}
+    (let [plan (pick-closest distances)
+          has-plan? (set (keys plan))
+          extra-ships (remove (fn [{:keys [ship]}]
+                                (has-plan? ship)) distances)]
+      (merge plan (repeatedly-pick-closest extra-ships)))))
 
 (defn target-planet
   [state ship planet]
@@ -38,8 +61,11 @@
   [{:keys [player-id] :as state}]
   (let [ships (get-in state [:ships-by-player-id player-id])
         grouped-planets (->> state :planets-by-id vals (group-by (comp boolean :has-owner)))
-        [owned unowned] (map grouped-planets [true false])]
-    (remove nil? (map (partial target-planet state) ships (cycle unowned)))))
+        [owned unowned] (map grouped-planets [true false])
+        plan (repeatedly-pick-closest (all-distances ships unowned))]
+    (remove nil? (map (fn [[ship planet]]
+                        (target-planet state ship planet))
+                      plan))))
 
 ;; get all ship/planet combinations for undocked ships
 ;; min-by sum of the distance
